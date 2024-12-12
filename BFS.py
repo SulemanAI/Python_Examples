1f3315be114028eb5f40003eec4c339ed766499f
SulemanAI <muhammadsuleman0759@gmail.com>
2024-12-12 10:29:29 +0500

    BFS algorithm in python

---
+++ BFS.py
@@ -0,0 +1,30 @@
+graph = {
+   # 'node' : 'neighbour'
+    '5':['3','7'],
+    '3':['2','4'],
+    '7':['8'],
+    '2':[],
+    '4':['8'],
+    '8':[]
+}
+
+
+visited = [] # list for visited nodes
+queue= []    # Initialize a queue
+
+def bfs(visited,graph,node): #Function fo BFS
+    visited.append(node)
+    queue.append(node)
+
+    while queue:  # Creating loop to visit each node
+        m = queue.pop(0)
+        print(m,end=" ")
+
+        for neighbour in graph[m]:
+            if neighbour not in visited:
+                visited.append(neighbour)
+                queue.append(neighbour)
+
+#Driver code
+print("Following is the Breadth-First Search")
+bfs(visited,graph,'5')
